92cc8be8bd341fafbeb52521c1d2a66660e78479
lnay <luke@grahamnaylor.net>
2023-04-03 23:33:47 +0100

    Switch to search-based filtering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,21 @@
   import DarkToggle from "../lib/DarkToggle.svelte";
   import {tests} from "./results.json";
 
-  let test_filter = (_test) => true;
-
-
+  $: test_filter = (test) => test.name.match(pattern);
+  let pattern = ".*";
 </script>
 
 <DarkToggle/>
 <br/>
-<button on:click={
-	()=>{test_filter = (_test) => true;}
-}>all</button>
-<button on:click={
-	()=>{test_filter = (test) => test.name.match(/^1/);}
-}>Task 1</button>
-<button on:click={
-	()=>{test_filter = (test) => test.name.match(/^2/);}
-}>Task 2</button>
-<button on:click={
-	()=>{test_filter = (test) => test.name.match(/^3/);}
-}>Task 3</button>
+
+<button on:click={ ()=>{pattern = ".*";} }>all</button>
+<button on:click={ ()=>{pattern = "^1";} }>Task 1</button>
+<button on:click={ ()=>{pattern = "^2";} }>Task 2</button>
+<button on:click={ ()=>{pattern = "^3";} }>Task 3</button>
+<br/>
+
+<label>
+search: <input bind:value={pattern}/>
+</label>
 
 <Result {tests} {test_filter} />
