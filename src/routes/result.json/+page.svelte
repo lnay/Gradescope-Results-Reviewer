a59b041199b1c154ace4ffe861575919560a029d
lnay <luke@grahamnaylor.net>
2023-04-04 12:28:50 +0100

    Move results.json viewer to diff path

---
+++ src/routes/result.json/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Result from "../../lib/Result.svelte";
+  import DarkToggle from "../../lib/DarkToggle.svelte";
+  import {tests} from "../results.json";
+
+  $: test_filter = (test) => test.name.match(pattern);
+  let pattern = ".*";
+</script>
+
+<DarkToggle/>
+<br/>
+
+<button on:click={ ()=>{pattern = ".*";} }>all</button>
+<button on:click={ ()=>{pattern = "^1";} }>Task 1</button>
+<button on:click={ ()=>{pattern = "^2";} }>Task 2</button>
+<button on:click={ ()=>{pattern = "^3";} }>Task 3</button>
+<br/>
+
+<label>
+search: <input bind:value={pattern}/>
+</label>
+<br/>
+<Result {tests} {test_filter}/>
