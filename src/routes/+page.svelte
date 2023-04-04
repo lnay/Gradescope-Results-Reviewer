2f4ad5db60ea06306cd44ef72fbf525df5b49bc3
lnay <luke@grahamnaylor.net>
2023-04-04 13:25:50 +0100

    Add View for all submissions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,33 @@
 <script>
-  import Result from "../lib/Result.svelte";
+  import { onMount } from 'svelte';
   import DarkToggle from "../lib/DarkToggle.svelte";
-  import {tests} from "./results.json";
+  import yaml from "js-yaml";
 
-  $: test_filter = (test) => test.name.match(pattern);
-  let pattern = ".*";
+  let submissions_data = null;
+
+  onMount(async () => {
+		const res = await fetch(`submissions/submission_metadata.yml`);
+		let submissions_yaml = await res.text();
+    submissions_data = yaml.load(submissions_yaml);
+	});
 </script>
 
 <DarkToggle/>
 <br/>
 
-<button on:click={ ()=>{pattern = ".*";} }>all</button>
-<button on:click={ ()=>{pattern = "^1";} }>Task 1</button>
-<button on:click={ ()=>{pattern = "^2";} }>Task 2</button>
-<button on:click={ ()=>{pattern = "^3";} }>Task 3</button>
-<br/>
-
-<label>
-search: <input bind:value={pattern}/>
-</label>
-<br/>
-
-<Result {tests} {test_filter} />
+{#if submissions_data}
+  <table>
+  <tr>
+  <th> submission number </th>
+  <th> name </th>
+  <th> score </th>
+  </tr>
+  {#each Object.entries(submissions_data) as [key, value]}
+    <tr>
+      <td>{key.substring(11)}</td>
+      <td>{value[":submitters"][0][":name"]}</td>
+      <td>{value[":results"].score}</td>
+    </tr>
+  {/each}
+  </table>
+{/if}
