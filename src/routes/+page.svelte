2d8fc986e8b4c4a9a17e5fa52f612adf9d4c726a
lnay <luke@grahamnaylor.net>
2023-04-24 22:43:12 +0100

    Add types to some objects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,55 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import type { ComponentType } from "svelte";
   import DarkToggle from "../lib/DarkToggle.svelte";
   import SubmissionIndividual from "../lib/SubmissionIndividual.svelte";
   import PythonFileView from "../lib/PythonFileView.svelte";
   import JupyterNotebookView from "../lib/JupyterNotebookView.svelte";
   import yaml from "js-yaml";
 
-  let focused_submission = "";
-  let focused_file_path = "";
-  let focused_file_component = PythonFileView;
+  let focused_submission: 
+  {
+    num: string,
+    dir: string,
+    details: any,
+  } | null = null;
+
+  let focused_file_path: string | null = null;
+  let focused_file_component: ComponentType = PythonFileView;
 
   let jupyter_server_url = "http://localhost:8888/";
   let jupyter_server_token = "";
 
-  const SUBMISSION_FILES = [
+  const SUBMISSION_FILES:
+  {
+    name: string,
+    label: string,
+    viewer_component: ComponentType
+  }[] = [
     {
       name: "task1.py",
+      label: "Task 1",
       viewer_component: PythonFileView,
     },
     {
       name: "task2.py",
+      label: "Task 2",
       viewer_component: PythonFileView,
     },
     {
       name: "task3.py",
+      label: "Task 3",
       viewer_component: PythonFileView,
     },
     {
       name: "A1.ipynb",
+      label: "Notebook",
       viewer_component: JupyterNotebookView,
     },
   ]
 
-  let submissions_data = null;
+  let submissions_data: any = null;  // TODO: type this, but it's a pain <- Copilot generated this comment
+
   onMount(async () => {
 		const res = await fetch(`submissions/submission_metadata.yml`);
 		let submissions_yaml = await res.text();
